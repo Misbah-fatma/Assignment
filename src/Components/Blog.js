e68d51ee318426709af48dc6cf245c268c9c6758
Misbah Fatma <misbah7370@gmail.com>
2024-10-14 13:21:03 +0530

    message

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,148 +1,148 @@
-import React from 'react'
+import React from 'react';
 
 const Blog = () => {
   return (
     <>
-    <section class="normal-breadcrumb set-bg">
-  
-          <div class="container">
-              <div class="row">
-                  <div class="col-lg-12 text-center">
-                  <img src="/assets/img/normal-breadcrumb.jpg" alt="Blog Image"/>
-                      <div class="normal__breadcrumb__text">
-                          <h2> Blog</h2>
-                          <p>Welcome to the official Anime blog.</p>
-                      </div>
-                  </div>
+      <section className="normal-breadcrumb set-bg">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <img src="/assets/img/normal-breadcrumb.jpg" alt="Anime Blog Banner" />
+              <div className="normal__breadcrumb__text">
+                <h2>Blog</h2>
+                <p>Welcome to the official Anime blog.</p>
               </div>
+            </div>
           </div>
-      </section>
-      <section class="blog spad">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="blog__item">
-                            <img src="/assets/img/blog/blog-1.jpg" alt="Blog Image 1"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Yuri Kuma Arashi Viverra Tortor Pharetra</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="blog__item small__item">
-                            <img src="/assets/img/blog/blog-4.jpg" alt="Blog Image 4"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Bok no Hero Academia Season 4 – 18</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="blog__item small__item">
-                            <img src="/assets/img/blog/blog-5.jpg" alt="Blog Image 5"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Fate/Stay Night: Untimated Blade World</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-12">
-                        <div class="blog__item">
-                            <img src="/assets/img/blog/blog-7.jpg" alt="Blog Image 7"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Housekishou Richard shi no Nazo Kantei Season 08 - 20</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="blog__item small__item">
-                            <img src="/assets/img/blog/blog-10.jpg" alt="Blog Image 10"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Fate/Stay Night: Untimated Blade World</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="blog__item small__item">
-                            <img src="/assets/img/blog/blog-11.jpg" alt="Blog Image 11"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Building a Better LiA Drilling Down</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="blog__item small__item">
-                            <img src="/assets/img/blog/blog-2.jpg" alt="Blog Image 2"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Fate/Stay Night: Untimated Blade World</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="blog__item small__item">
-                            <img src="/assets/img/blog/blog-3.jpg" alt="Blog Image 3"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Building a Better LiA Drilling Down</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-12">
-                        <div class="blog__item">
-                            <img src="/assets/img/blog/blog-6.jpg" alt="Blog Image 6"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Yuri Kuma Arashi Viverra Tortor Pharetra</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="blog__item small__item">
-                            <img src="/assets/img/blog/blog-8.jpg" alt="Blog Image 8"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Bok no Hero Academia Season 4 – 18</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="blog__item small__item">
-                            <img src="/assets/img/blog/blog-9.jpg" alt="Blog Image 9"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Fate/Stay Night: Untimated Blade World</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-12">
-                        <div class="blog__item">
-                            <img src="/assets/img/blog/blog-12.jpg" alt="Blog Image 12"/>
-                            <div class="blog__item__text">
-                                <p><span class="icon_calendar"></span> 01 March 2020</p>
-                                <h4><a href="#">Yuri Kuma Arashi Viverra Tortor Pharetra</a></h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
-    </div>
-</section>
+      </section>
 
-          
-          </>
-  )
-}
+      <section className="blog spad">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="blog__item">
+                    <img src="/assets/img/blog/blog-1.jpg" alt="Yuri Kuma Arashi Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Yuri Kuma Arashi Viverra Tortor Pharetra</a></h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6">
+                  <div className="blog__item small__item">
+                    <img src="/assets/img/blog/blog-4.jpg" alt="Bok no Hero Academia Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Bok no Hero Academia Season 4 – 18</a></h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6">
+                  <div className="blog__item small__item">
+                    <img src="/assets/img/blog/blog-5.jpg" alt="Fate/Stay Night Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Fate/Stay Night: Unlimited Blade World</a></h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-12">
+                  <div className="blog__item">
+                    <img src="/assets/img/blog/blog-7.jpg" alt="Housekishou Richard Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Housekishou Richard shi no Nazo Kantei Season 08 - 20</a></h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6">
+                  <div className="blog__item small__item">
+                    <img src="/assets/img/blog/blog-10.jpg" alt="Fate/Stay Night Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Fate/Stay Night: Unlimited Blade World</a></h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6">
+                  <div className="blog__item small__item">
+                    <img src="/assets/img/blog/blog-11.jpg" alt="Building a Better LiA Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Building a Better LiA Drilling Down</a></h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-export default Blog
+            {/* Second Column */}
+            <div className="col-lg-6">
+              <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-6">
+                  <div className="blog__item small__item">
+                    <img src="/assets/img/blog/blog-2.jpg" alt="Fate/Stay Night Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Fate/Stay Night: Unlimited Blade World</a></h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6">
+                  <div className="blog__item small__item">
+                    <img src="/assets/img/blog/blog-3.jpg" alt="Building a Better LiA Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Building a Better LiA Drilling Down</a></h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-12">
+                  <div className="blog__item">
+                    <img src="/assets/img/blog/blog-6.jpg" alt="Yuri Kuma Arashi Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Yuri Kuma Arashi Viverra Tortor Pharetra</a></h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6">
+                  <div className="blog__item small__item">
+                    <img src="/assets/img/blog/blog-8.jpg" alt="Bok no Hero Academia Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Bok no Hero Academia Season 4 – 18</a></h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6">
+                  <div className="blog__item small__item">
+                    <img src="/assets/img/blog/blog-9.jpg" alt="Fate/Stay Night Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Fate/Stay Night: Unlimited Blade World</a></h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-12">
+                  <div className="blog__item">
+                    <img src="/assets/img/blog/blog-12.jpg" alt="Yuri Kuma Arashi Blog Post" />
+                    <div className="blog__item__text">
+                      <p><span className="icon_calendar"></span> 01 March 2020</p>
+                      <h4><a href="your-link-here">Yuri Kuma Arashi Viverra Tortor Pharetra</a></h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Blog;
